82764647e2492b1645f96052504efda980d19ed1
Skyler Dakota Lawton <skylerdakota@gmail.com>
2021-01-26 11:08:56 -0500

    added portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,71 +1,84 @@
-import React, { Component } from "react";
-import API from "../utils/API";
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 import Card from "../components/Card";
-import Alert from "../components/Alert";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
-class Discover extends Component {
-  state = {
-    image: "",
-    match: false,
-    matchCount: 0
-  };
-
-  // When the component mounts, load the next dog to be displayed
-  componentDidMount() {
-    this.loadNextDog();
-  }
-
-  handleBtnClick = event => {
-    // Get the data-value of the clicked button
-    const btnType = event.target.attributes.getNamedItem("data-value").value;
-    // Clone this.state to the newState object
-    // We'll modify this object and use it to set our component's state
-    const newState = { ...this.state };
-
-    if (btnType === "pick") {
-      // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-      newState.match = 1 === Math.floor(Math.random() * 5) + 1;
-
-      // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-      newState.matchCount = newState.match
-        ? newState.matchCount + 1
-        : newState.matchCount;
-    } else {
-      // If we thumbs down'ed the dog, we haven't matched with it
-      newState.match = false;
-    }
-    // Replace our component's state with newState, load the next dog image
-    this.setState(newState);
-    this.loadNextDog();
-  };
-
-  loadNextDog = () => {
-    API.getRandomDog()
-      .then(res =>
-        this.setState({
-          image: res.data.message
-        })
-      )
-      .catch(err => console.log(err));
-  };
-
-  render() {
-    return (
-      <div>
-        <h1 className="text-center">Make New Friends</h1>
-        <h3 className="text-center">
-          Thumbs up on any pups you'd like to meet!
-        </h3>
-        <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
-        <h1 className="text-center">
-          Made friends with {this.state.matchCount} pups so far!
-        </h1>
-        <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">
-          Yay! That Pup Liked You Too!!!
-        </Alert>
-      </div>
-    );
-  }
+function Portfolio() {
+  return (
+    <div>
+      <Header>
+        <Navbar>
+        </Navbar>
+      </Header>
+      <Main>
+        <Container>
+          <Row/>
+          <Col/>
+          <Card>
+            <Row>
+              <Col>
+                <Col size="md-3">
+                  <Link>href="https://personality-pups.herokuapp.com//" Personality Pup style="color: #6ea7d2;" </Link>
+                </Col>  
+                <Col size="md-3">
+                  <Link>style="color: #6ea7d2;" href="https://github.com/oxfordblucher/Project-2-Untitled" GitHub Repo </Link> 
+                </Col>
+                <img>src="assets/images/personalitypupquiz.png" alt="Gentrification Sucks" class=".img-fluid. max-width: 100%; height: auto;"</img>            
+              </Col>
+                <Col>
+                  <Col size="md-3">
+                    <Link>href="https://skylerdakota.github.io/Eat-a-Burger/" Eat-a-Burger style="color: #6ea7d2;" </Link>
+                  </Col>  
+                  <Col size="md-3">
+                    <Link>style="color: #6ea7d2;" href="https://github.com/skylerdakota/Eat-a-Burger" GitHub Repo</Link> 
+                  </Col>
+                  <img>src="assets/images/eataburger.png" alt="Eat-a-Burger"  class=".img-fluid. max-width: 100%; height: auto;"</img>            
+                </Col>
+                <Col>
+                  <Col size="md-3">
+                    <Link>href="https://nicolas-tomlinsonstjuste.github.io/Gentrification-Sucks/"Gentrification Sucks style="color: #6ea7d2;" </Link>
+                  </Col>  
+                  <Col size="md-3">
+                    <Link>style="color: #6ea7d2;" href="https://github.com/nicolas-tomlinsonstjuste/Gentrification-Sucks" GitHub Repo</Link> 
+                  </Col>
+                  <img>src="assets/images/gentrificationsucks.png" alt="Gentrification Sucks"  class=".img-fluid. max-width: 100%; height: auto;"</img>            
+                </Col>
+                <Col>
+                  <Col size="md-3">
+                    <Link>href="https://skylerdakota.github.io/Work-Day-Scheduler/" Workday Scheduler style="color: #6ea7d2;" </Link>
+                  </Col>  
+                  <Col size="md-3">
+                    <Link>style="color: #6ea7d2;" href="https://github.com/skylerdakota/Work-Day-Scheduler" GitHub Repo</Link> 
+                  </Col>
+                  <img>src="assets/images/scheduler.png" alt="Workday Scheduler"  class=".img-fluid. max-width: 100%; height: auto;"</img>            
+                </Col>
+                <Col>
+                  <Col size="md-3">
+                    <Link>href="https://skylerdakota.github.io/Password-Generator/" Password Generator style="color: #6ea7d2;" </Link>
+                  </Col>  
+                  <Col size="md-3">
+                    <Link>style="color: #6ea7d2;" href="https://github.com/skylerdakota/Password-Generator" GitHub Repo</Link> 
+                  </Col>
+                  <img>src="assets/images/passwordgenerator.png" alt="Password Generator"  class=".img-fluid. max-width: 100%; height: auto;"</img>            
+                </Col>
+                <Col>
+                  <Col size="md-3">
+                    <Link>href="https://skylerdakota.github.io/Password-Generator/" Password Generator style="color: #6ea7d2;" </Link>
+                  </Col>  
+                  <Col size="md-3">
+                    <Link>style="color: #6ea7d2;" href="https://github.com/skylerdakota/Work-Day-Scheduler" GitHub Repo</Link> 
+                  </Col>
+                  <img>src="assets/images/eataburger.png" alt="Workday Scheduler"  class=".img-fluid. max-width: 100%; height: auto;"</img>            
+                </Col>
+              </Row>
+          </Card>
+        </Container>
+      </Main>
+    </div>
+  );
 }
 
-export default Discover;
+export default Portfolio;
